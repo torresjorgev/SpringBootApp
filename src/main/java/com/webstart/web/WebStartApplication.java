a798475b79248cbe4c859965386ae4f31a68bcdd
Jorge Torres <jorgevtorresr@gmail.com>
2018-05-05 18:05:06 -0500

    Failed to make IDEA work, moving right along, sticking to eclipse

--- src/main/java/com/webstart/web/WebStartApplication.java
+++ src/main/java/com/webstart/web/WebStartApplication.java
@@ -1,11 +1,13 @@
 package com.webstart.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration 
+@Configuration
 @ComponentScan
+//@SpringBootApplication
 public class WebStartApplication {
 
 	public static void main(String[] args) {
